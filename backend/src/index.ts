fe7e46c5f4c7af5d2fe12cccda869d802460447a
shubham-sanvariya <shubhamsanvariyait.com>
2023-11-29 22:15:20 +0530

    adding midleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,4 +2,8 @@ import  express  from "express";
 
 const app = express();
 
+// midlewares
+app.use(express.json());
+
+// connections and listneres
 app.listen(5000,() => console.log("server open"));
