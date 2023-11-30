382bb1dd15a9b9457a81ec1d654aa4a949f6aa08
shubham-sanvariya <shubhamsanvariyait.com>
2023-11-30 20:46:24 +0530

    adding user signup route

---
+++ backend/dist/controllers/user-controllers.js
@@ -0,0 +1,27 @@
+import User from '../models/User.js';
+import { hash } from 'bcrypt';
+export const getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({ message: "Ok", users });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+};
+export const userSignup = async (req, res, next) => {
+    try {
+        // uesr signup
+        const { name, email, password } = req.body;
+        const hashedPassword = await hash(password, 10);
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
+        return res.status(200).json({ message: "Ok", id: user._id.toString() });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+};
+//# sourceMappingURL=user-controllers.js.map
---
+++ backend/dist/db/connection.js
@@ -0,0 +1,21 @@
+import { connect, disconnect } from "mongoose";
+async function connectToDatabase() {
+    try {
+        await connect(process.env.MONGODB_URL);
+    }
+    catch (error) {
+        console.log(error);
+        throw new Error("Cannot connect to mongodb");
+    }
+}
+async function disconnectFromDatabase() {
+    try {
+        await disconnect();
+    }
+    catch (error) {
+        console.log(error);
+        throw new Error("Cannot Disconnect to mongodb");
+    }
+}
+export { connectToDatabase, disconnectFromDatabase };
+//# sourceMappingURL=connection.js.map
