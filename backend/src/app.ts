4a1072d5a56096d633e64a1bd826ec2686e06f59
shubham-sanvariya <shubhamsanvariyait.com>
2023-11-30 12:08:41 +0530

    using morgan

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,15 @@
 import  express  from "express";
 import { config } from "dotenv";
+import morgan from 'morgan';
 config();
 const app = express();
 
 // midlewares
 app.use(express.json());
+
+// remove it in production
+app.use(morgan("dev"));
+
+app.use("/api/v1");
 
 export default app;
