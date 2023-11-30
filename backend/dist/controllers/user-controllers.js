8d07e2c25d358b247cc12cb8d29d3d1fa8aa359e
shubham-sanvariya <shubhamsanvariyait.com>
2023-11-30 22:38:18 +0530

    correcting the status code

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -17,7 +17,7 @@ export const userSignup = async (req, res, next) => {
         const hashedPassword = await hash(password, 10);
         const user = new User({ name, email, password: hashedPassword });
         await user.save();
-        return res.status(200).json({ message: "Ok", id: user._id.toString() });
+        return res.status(201).json({ message: "Ok", id: user._id.toString() });
     }
     catch (error) {
         console.log(error);
