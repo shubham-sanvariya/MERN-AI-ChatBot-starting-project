1eec13c86a542c84411720aa663f8f756f6e7f92
shubham-sanvariya <shubhamsanvariyait.com>
2023-11-30 12:08:28 +0530

    adding connectToDatabase

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,11 @@
 import app from './app.js';
+import { connectToDatabase } from './db/connection.js';
 
 // connections and listneres
-app.listen(5000,() => console.log("server open"));
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+        app.listen(PORT, () =>
+         console.log("server open & connected to database "));
+    })
+    .catch((err) => console.log(err));
