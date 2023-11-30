0cd55c0133f4c593d2d807705b0782518339f2bb
shubham-sanvariya <shubhamsanvariyait.com>
2023-11-30 18:13:12 +0530

    adding get req

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,4 +1,10 @@
-import express from "express";
-const app = express();
-app.listen(5000, () => console.log("server open"));
+import app from './app.js';
+import { connectToDatabase } from './db/connection.js';
+// connections and listneres
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+    app.listen(PORT, () => console.log("server open & connected to database "));
+})
+    .catch((err) => console.log(err));
 //# sourceMappingURL=index.js.map
