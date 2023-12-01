685f6328a9e99c6d3a907968ecd5019784e1a0fe
shubham-sanvariya <shubhamsanvariyait.com>
2023-12-01 10:55:00 +0530

    adding login validator & userLogin fn

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,5 +1,5 @@
 import User from '../models/User.js';
-import { hash } from 'bcrypt';
+import { compare, hash } from 'bcrypt';
 export const getAllUsers = async (req, res, next) => {
     try {
         const users = await User.find();
@@ -14,10 +14,32 @@ export const userSignup = async (req, res, next) => {
     try {
         // uesr signup
         const { name, email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser)
+            return res.status(401).send("User already registered");
         const hashedPassword = await hash(password, 10);
         const user = new User({ name, email, password: hashedPassword });
         await user.save();
         return res.status(201).json({ message: "Ok", id: user._id.toString() });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+};
+export const userLogin = async (req, res, next) => {
+    try {
+        // uesr login
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).send("User not registered");
+        }
+        const isPasswordCorrect = await compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(403).send("Incorrect Password");
+        }
+        return res.status(200).json({ message: "Ok", id: user._id.toString() });
     }
     catch (error) {
         console.log(error);
