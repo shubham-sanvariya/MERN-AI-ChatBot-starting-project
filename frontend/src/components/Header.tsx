c8ff81b205f478f0c4bb9b0414e31f458e4ba1f8
shubham-sanvariya <shubhamsanvariyait.com>
2023-12-02 17:14:40 +0530

    adding Logo component in header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,15 @@
+import { AppBar, Toolbar } from '@mui/material'
 import React from 'react'
+import Logo from './shared/Logo'
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+  return <AppBar
+   sx={{ bgcolor: "transparent", position: "static",
+    boxShadow: "none"}}>
+    <Toolbar sx={{ display: "flex" }}>
+      <Logo />
+    </Toolbar>
+  </AppBar>
 }
 
 export default Header
