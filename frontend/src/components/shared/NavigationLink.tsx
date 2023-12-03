a0e195328b9933bf8891e55901ead5f42d420452
shubham-sanvariya <shubhamsanvariyait.com>
2023-12-03 11:51:59 +0530

    adding NavigationLink with custom props

---
+++ frontend/src/components/shared/NavigationLink.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+type Props = {
+    to: string;
+    bg: string;
+    text: string;
+    textcolor: string;
+    onclick?:() => Promise<void>;
+}
+const NavigationLink = (props: Props) => {
+    return(
+        <Link
+            to={props.to}
+            style={{ background: props.bg,
+                    color: props.textcolor}}
+        >
+            {props.text}
+    </Link>
+    );
+};
+
+export default NavigationLink;
