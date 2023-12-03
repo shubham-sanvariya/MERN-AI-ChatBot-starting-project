6bfd56e42f308cc71cd3c7be40240c21cb9b867a
shubham-sanvariya <shubhamsanvariyait.com>
2023-12-03 11:52:41 +0530

    implementing NavigationLink in Headers

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,11 @@
 import { AppBar, Toolbar } from '@mui/material'
 import React from 'react'
 import Logo from './shared/Logo'
+import { useAuth } from '../context/AuthContext'
+import NavigationLink from './shared/NavigationLink'
 
 const Header = () => {
+  const auth = useAuth();
   return <AppBar
     sx={{
       bgcolor: "transparent", position: "static",
@@ -10,6 +13,40 @@ const Header = () => {
     }}>
     <Toolbar sx={{ display: "flex" }}>
       <Logo />
+      <div>
+        {auth?.isLoggedIn ? (
+          <>
+            <NavigationLink
+              bg="#00fffc"
+              to='/chat'
+              text='Go To Chat'
+              textcolor='black'
+               />
+            <NavigationLink
+              bg='#51538f'
+              textcolor='white'
+              to='/'
+              text="logout"
+              onclick={auth.logout}
+            />
+          </>
+        ) : (
+            <>
+              <NavigationLink
+                bg="#00fffc"
+                to='/login'
+                text='Login'
+                textcolor='black'
+              />
+              <NavigationLink
+                bg='#51538f'
+                to='/signup'
+                text="Signup"
+                textcolor='white'
+              />
+            </>
+        )}
+      </div>
     </Toolbar>
   </AppBar>
 }
